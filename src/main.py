4ae4b14da8cba86b86296f55109efbf78b820942
Bruno Vinicius de Oliveira e Souza <brunovos@uninove.br>
2022-05-02 20:14:00 -0300

    aff

--- src/main.py
+++ src/main.py
@@ -4,14 +4,27 @@ import string
 from unidecode import unidecode
 from fuzzywuzzy import fuzz
 
+import nltk
+nltk.download('stopwords')
+
+stopwords = nltk.corpus.stopwords.words('portuguese')
+stopwords = [stopword for stopword in stopwords if len(stopword) > 2]
+
+
+
+
+
 
 df = pd.read_excel('./data/dados_interacoes_usuarios_identificados.xlsx')
-df_test = pd.read_excel('./data/dados_interacoes_usuarios_identificados.xlsx', dtype='str')
 
+df_test = pd.read_excel('./data/dados_interacoes_usuarios_identificados.xlsx', dtype='str')
 n_extensos = pd.read_csv('./data/numeros_extenso.txt', sep=' - ', names=['NUMERO', 'EXTENSO'])
 
-
 n_extensos = n_extensos.iloc[:11]
+
+
+
+
 
 
 def substitui_numero_extenso(frase_cliente):
@@ -21,6 +34,9 @@ def substitui_numero_extenso(frase_cliente):
             frase_cliente = frase_cliente.replace(n_extensos['NUMERO'][i], n_extensos['EXTENSO'][i])
 
     return frase_cliente
+
+
+df_test['frase_cliente'] = df_test['frase_cliente'].str.replace('0', '')
 
 
 df_test['frase_cliente'] = df_test['frase_cliente'].apply(substitui_numero_extenso)
@@ -43,7 +59,7 @@ df_test['frase_cliente'] = df_test['frase_cliente'].apply(unidecode)
 
 
 
-df_test['frase_cliente'] = df_test['frase_cliente'].str.replace('0', '')
+
 
 
 
@@ -53,11 +69,8 @@ num_teste = n_extensos['EXTENSO'].apply(unidecode)
 num_teste = num_teste.tolist()
 
 
-
-
-
 #ELIMINANDO FUZZY
-def corrige_numeros(frase_cliente, THRESHOLD = 70):
+def corrige_numeros(frase_cliente, THRESHOLD = 80):
 
     frase_cliente = frase_cliente.split(' ')
 
@@ -75,21 +88,61 @@ def corrige_numeros(frase_cliente, THRESHOLD = 70):
     return frase_cliente
 
 
-
-
-df_test['frase_cliente'].apply(corrige_numeros)
-
+df_test['frase_cliente'] = df_test['frase_cliente'].apply(corrige_numeros)
 
 
 
-df_test['frase_cliente'][27]
 
+
+def eliminando_stopwords(frase):
+
+    tokens = frase.split(' ')
+    for k in range(len(tokens)):
+        for stopword in stopwords:
+            if tokens[k] == stopword:
+                tokens[k] = 'ELIMINA'
+
+
+    frase = ' '.join(tokens)
+    frase = frase.replace(' ELIMINA', '')
+    frase = frase.replace('ELIMINA ', '')
+
+    return frase
+
+df_test['frase_cliente'] = df_test['frase_cliente'].apply(eliminando_stopwords)
 
 
 
 #--------------------
 
 lb = LabelEncoder()
+lb2 = LabelEncoder()
 
 df_test['resposta_assistente_virtual'] = lb.fit_transform(df_test['resposta_assistente_virtual'])
+df_test['intent'] = lb2.fit_transform(df_test['intent'])
+
+
+
+
+
+qnt_int = len(df_test) / 2
+
+
+
+
+x = []
+y = []
+
+frase_1 = df_test[0:2].iloc[0]['frase_cliente']
+frase_2 = df_test[0:2].iloc[1]['frase_cliente']
+
+intencao_1 = df_test[0:2].iloc[0]['intent']
+intencao_2 = df_test[0:2].iloc[1]['intent']
+
+
+x.append(frase_1 + ' ' + frase_2)
+y.append([intencao_1, intencao_2])
+
+
+
 
