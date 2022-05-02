c4ae8b29353304b7d1c3ea0d86f15a9aedc6e9e2
Bruno Vinicius de Oliveira e Souza <brunovos@uninove.br>
2022-05-02 16:52:20 -0300

    metade do trabalho feito

--- src/main.py
+++ src/main.py
@@ -1,21 +1,41 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
-
+import string
+from unidecode import unidecode
 
 df = pd.read_excel('./data/dados_interacoes_usuarios_identificados.xlsx')
-
-df_test = pd.read_excel('./data/dados_interacoes_usuarios_identificados.xlsx')
-
-
+df_test = pd.read_excel('./data/dados_interacoes_usuarios_identificados.xlsx', dtype='str')
 
 n_extensos = pd.read_csv('./data/numeros_extenso.txt', sep=' - ', names=['NUMERO', 'EXTENSO'])
 
 
-n_extensos = n_extensos.iloc[:11]
+def substitui_numero_extenso(frase_cliente):
+
+    for i in range(len(n_extensos)):
+        if n_extensos['NUMERO'][i] in frase_cliente:
+            frase_cliente = frase_cliente.replace(n_extensos['NUMERO'][i], n_extensos['EXTENSO'][i])
+
+    return frase_cliente
 
 
-def substitui_extenso(frase):
-    
+df_test['frase_cliente'] = df_test['frase_cliente'].apply(substitui_numero_extenso)
+
+
+
+pontuacoes = string.punctuation
+
+def elimina_pontos(frase):
+    for pontuacao in pontuacoes:
+        frase = frase.replace(pontuacao, '')
+    return frase
+
+
+df_test['frase_cliente'].apply(elimina_pontos)
+
+elimina_pontos = lambda x: [x.replace(pontuacao, '') for pontuacao in pontuacoes]
+
+
+
 
 
 
@@ -23,5 +43,3 @@ lb = LabelEncoder()
 
 df_test['resposta_assistente_virtual'] = lb.fit_transform(df_test['resposta_assistente_virtual'])
 
-
-df_test.iloc[0]
