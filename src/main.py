4b5d986d1d4f4014592378dcef3b6d765441b6e0
Bruno Vinicius de Oliveira e Souza <brunovos@uninove.br>
2022-05-02 21:54:55 -0300

    dfhbsdif

--- src/main.py
+++ src/main.py
@@ -10,9 +10,6 @@ import nltk
 nltk.download('stopwords')
 
 stopwords = nltk.corpus.stopwords.words('portuguese')
-stopwords = [stopword for stopword in stopwords if len(stopword) > 2]
-
-
 
 
 
@@ -24,9 +21,9 @@ n_extensos = pd.read_csv('./data/numeros_extenso.txt', sep=' - ', names=['NUMERO
 
 n_extensos = n_extensos.iloc[:11]
 
+n_extensos['EXTENSO'] = n_extensos['EXTENSO'].apply(unidecode)
 
-
-
+df_test['frase_cliente'] = df_test['frase_cliente'].apply(unidecode)
 
 
 def substitui_numero_extenso(frase_cliente):
@@ -61,18 +58,20 @@ df_test['frase_cliente'] = df_test['frase_cliente'].apply(unidecode)
 
 
 
-
-
-
-
-
-
 num_teste = n_extensos['EXTENSO'].apply(unidecode)
+
+
+
+
 num_teste = num_teste.tolist()
 
 
+
+
+
+
 #ELIMINANDO FUZZY
-def corrige_numeros(frase_cliente, THRESHOLD = 70):
+def corrige_numeros(frase_cliente, THRESHOLD = 75):
 
     frase_cliente = frase_cliente.split(' ')
 
@@ -82,12 +81,13 @@ def corrige_numeros(frase_cliente, THRESHOLD = 70):
 
             resultado_fuzz = fuzz.ratio(frase_cliente[i], num_teste[j])
 
-            if resultado_fuzz > THRESHOLD:
+            if resultado_fuzz >= THRESHOLD:
                 frase_cliente[i] = num_teste[j]
 
     frase_cliente = ' '.join(frase_cliente)
 
     return frase_cliente
+
 
 
 df_test['frase_cliente'] = df_test['frase_cliente'].apply(corrige_numeros)
@@ -110,6 +110,7 @@ def eliminando_stopwords(frase):
     frase = ' '.join(tokens)
     frase = frase.replace(' ELIMINA', '')
     frase = frase.replace('ELIMINA ', '')
+    frase = frase.replace('ELIMINA', '')
 
     return frase
 
@@ -129,26 +130,27 @@ df_test['intent'] = lb2.fit_transform(df_test['intent'])
 
 
 
-qnt_int = int(len(df_test) / 2)
-
 
 
 x = []
 y = []
 
-
-for i in range(0, len(df_test), 2):
-    print(i)
-    print(i+1)
-    print('--------------')
-
-
-
-
-
 for i in range(0, len(df_test), 2):
     frase_1 = df_test['frase_cliente'][i]
     frase_2 = df_test['frase_cliente'][i+1]
+
+
+    frase_2_splited = frase_2.split()
+
+
+
+
+    for j in range(len(frase_2_splited)):
+
+        if frase_2_splited[j] in n_extensos['EXTENSO'].tolist():
+            
+
+
 
     intencao_2 = df_test['intent'][i+1]
 
@@ -157,26 +159,23 @@ for i in range(0, len(df_test), 2):
 
 
 
+y
 
 
 
-
-
-pd.DataFrame(y).drop_duplicates()
 
 
 vectorizer = CountVectorizer()
 X = vectorizer.fit_transform(x)
 
 
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import GaussianNB
 
 
-nb_tfidf = MultinomialNB()
+nb_tfidf = GaussianNB()
 nb_tfidf.fit(X, y)
 
 y_pred = nb_tfidf.predict(X)
-
 
 
 pd.DataFrame({'y':y, 'pred':y_pred})
@@ -184,4 +183,4 @@ pd.DataFrame({'y':y, 'pred':y_pred})
 
 
 
-x[2]
+
